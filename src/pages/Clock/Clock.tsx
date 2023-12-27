7fb681d6862239a43c00b02e4bcd5087a63ed271
nm1008 <mallari08nikko@yahoo.com>
2023-12-28 04:12:13 +0800

    fixed bug migrating to typescript

--- src/pages/Clock/Clock.tsx
+++ src/pages/Clock/Clock.tsx
@@ -1,5 +1,57 @@
+import { useEffect, useState } from "react";
+
 const Clock = () => {
-  return <div>clock page</div>;
+  const [date, setDate] = useState<string | number>("");
+  const [month, setMonth] = useState<string | number>("");
+  const [year, setYear] = useState<string | number>("");
+  const [hours, setHours] = useState<string | any>("");
+  const [minutes, setMinutes] = useState<string>("");
+  const [seconds, setSeconds] = useState<string>("");
+
+  useEffect(() => {
+    const updateTime = () => {
+      const d = new Date();
+      setDate(d.getDate());
+      setMonth(d.getMonth());
+      setYear(d.getFullYear());
+      setHours(d.getHours().toString());
+
+      setMinutes(() => {
+        if (d.getMinutes() < 10) {
+          return `0${d.getMinutes()}`;
+        } else {
+          return d.getMinutes().toString();
+        }
+      });
+
+      setSeconds(() => {
+        if (d.getSeconds() < 10) {
+          return `0${d.getSeconds()}`;
+        } else {
+          return d.getSeconds().toString();
+        }
+      });
+    };
+
+    setInterval(updateTime, 1000);
+    updateTime();
+  }, [hours, minutes, seconds]);
+
+  return (
+    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0">
+      <div className=" space-y-4 md:space-y-6 sm:p-8">
+        <div className="flex gap-10 justify-between flex-col items-center py-5">
+          <h1 className="text-3xl font-bold ">
+            {hours < 10 ? `${hours % 12}` : `0${hours % 12}`}:{minutes}:
+            {seconds} {hours < 12 ? "AM" : "PM"}
+          </h1>
+          <h1>
+            {month}/{date}/{year}
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Clock;
