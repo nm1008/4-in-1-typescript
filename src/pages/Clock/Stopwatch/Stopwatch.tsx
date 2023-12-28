2b2c5db38fbf139e9cc2f71218c0c27a29d58a97
nm1008 <mallari08nikko@yahoo.com>
2023-12-28 09:14:33 +0800

    fixed some minor bugs in stopwatch component

--- src/pages/Clock/Stopwatch/Stopwatch.tsx
+++ src/pages/Clock/Stopwatch/Stopwatch.tsx
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 import Button from "../../../components/Button";
 
 const Stopwatch = () => {
-  const [time, setTime] = useState<number | any>(0);
+  const [time, setTime] = useState<number | null>(0);
   const [timer, setTimer] = useState<number | null>(null);
 
   useEffect(() => {
     if (timer !== null) {
       const intervalId = setInterval(() => {
-        setTime((prevTime: number) => prevTime + 10);
+        setTime((prevTime: number | null) => {
+          if (prevTime !== null) {
+            return prevTime + 10;
+          } else {
+            return null; // Handle the null case
+          }
+        });
       }, 10);
       return () => clearInterval(intervalId);
     }
@@ -27,27 +33,31 @@ const Stopwatch = () => {
     setTimer(null);
   };
 
-  const formatTime = (milliseconds: number) => {
+  const formatTime = (milliseconds: number | null) => {
+    if (milliseconds === null) {
+      return '00:00:00.000';
+    }
+  
     const totalMilliseconds = milliseconds;
     const millisecondsPart = Math.floor(totalMilliseconds % 1000);
     const seconds = Math.floor((totalMilliseconds / 1000) % 60);
     const minutes = Math.floor((totalMilliseconds / (1000 * 60)) % 60);
     const hours = Math.floor((totalMilliseconds / (1000 * 60 * 60)) % 24);
-
+  
     return `${hours.toString().padStart(2, "0")}:${minutes
       .toString()
       .padStart(2, "0")}:${seconds
       .toString()
       .padStart(2, "0")}.${millisecondsPart.toString().padStart(3, "0")}`;
   };
-
+  
   return (
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-lg xl:p-0">
       <div className=" space-y-4 h md:space-y-6 sm:p-8">
         <div className="flex gap-10 h-52 justify-center flex-col items-center md:h-58 ">
           <h1 className="text-4xl font-bold">{formatTime(time)}</h1>
           <div className="flex gap-3">
-            {timer > 0 ? (
+            {!timer ? (
               <>
                 <Button color={"bg-red-600"} onClick={handlePauseStopwatch}>
                   Pause
